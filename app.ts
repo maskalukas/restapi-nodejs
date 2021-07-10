224dd1cd9c3fe486842a6a0df0665dc25a558340
maskalukas <maska.lukas@post.cz>
2021-07-10 10:02:14 +0200

    products-route

--- app.ts
+++ app.ts
@@ -1,8 +1,13 @@
+export {};
 const express = require("express");
 const app = express();
 const port = 3000;
 
+const products = require("./routes/products");
 
 app.listen(port,() => {
-    console.log("Started");
+    console.log("Started...");
 });
+
+// routes - products
+app.use('/products', products);
---
+++ routes/products.ts
@@ -0,0 +1,9 @@
+const express = require("express"),
+    router = express.Router();
+
+
+
+
+
+
+module.exports = test;
